35234988d4f5aa97404832ba070d6b0aa125a905
Kashish Mishra <kashishmishra9911@gmail.com>
2021-05-10 19:50:45 +0530

    second commit

--- socialWebsite/settings.py
+++ socialWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'i7bh(1(af**mwt9x&r)6ga@_h69-#3oi6oj1370m*sr)bdi$k5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
